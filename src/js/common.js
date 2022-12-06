7d9c9e0d15a9726faf3d6f7c2606f9d4456bc667
Dhiran07 <97387022+Dhiran07@users.noreply.github.com>
2022-12-06 17:42:55 +0530

    Adding SRC folder

---
+++ src/js/common.js
@@ -0,0 +1,20 @@
+const logoutBtn = document.getElementById('logoutBtn');
+const userIntro = document.getElementById("userIntro");
+
+//const BASE_URL = "https://ecommce-be.herokuapp.com/ecomm/api/v1"
+const BASE_URL = "http://localhost:3000/ecomm/api/v1/"
+
+
+logoutBtn.addEventListener('click', logoutFn);
+
+
+function logoutFn() {
+	localStorage.removeItem('username');
+	window.location.href = "login.html";
+}
+
+if (!localStorage.getItem('username')) {
+	window.location.href = "login.html";
+} else {
+	userIntro.innerText = "Hi " + localStorage.getItem('username');
+}
